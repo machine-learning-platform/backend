6cf6d956be15af52cd27c3a23f73434232891dd2
Ziyu Chen <zc839@nyu.edu>
2020-02-09 01:19:18 +0800

    Added basic regression functionality.

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
-
+from flask import Flask, request, jsonify
+import numpy as np
+from sklearn.linear_model import LinearRegression
 from flask.logging import default_handler
 
 
@@ -9,10 +10,16 @@ app.logger.removeHandler(default_handler)
 logging.basicConfig(level=logging.DEBUG)
 @app.route('/regression', methods=['POST'])
 def regression():
-    DV = request.get_json()['DV']
-    IV = request.get_json()['IV']
-    columns = request.get_json()['columns']
-    rows = request.get_json()['rows']
-    #app.logger.info(str(request.get_json()))
-    
-    return 'Hello, world!'
+    # DV = request.get_json()['DV']
+    # IV = request.get_json()['IV']
+    X = np.array(request.get_json()['X'][:-1])
+    y = np.array(request.get_json()['y'][:-1])
+    reg = LinearRegression().fit(X,y)
+    app.logger.info(reg.score(X, y))
+    app.logger.info(reg.coef_) 
+    app.logger.info(reg.intercept_)   
+    return jsonify({
+        'score': reg.score(X, y),
+        'coefficients': list(reg.coef_),
+        'intercept': reg.intercept_
+    })
